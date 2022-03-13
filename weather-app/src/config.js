9ef1cad3bb42d97e713fc235bb9a4f3cbbe52620
lmcgeath <49741190+lmcgeath@users.noreply.github.com>
2022-03-12 16:15:00 -0800

    Update config.js

--- weather-app/src/config.js
+++ weather-app/src/config.js
@@ -1,8 +1,5 @@
-//actual key for weather api
-const key = 'a5b3187bce5f5c0d15914e60c08caee3';
-
-//temporary for testing
-// const apiKey = 'bceff109e16f3d85844791d1e3bf63b8';
+//actual key for weather api (re-generated 03/12/2022)
+const key = 'c46e80ae10d2b0f66ea45e9fc3d2cda0';
 
 
 export default key;
